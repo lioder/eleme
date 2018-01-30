03e1801621c76b751fd0285346754a299050ce84
161250053 <161250053@smail.nju.edu.cn>
2018-01-30 22:04:40 +0800

    dev(goods): add food-content's style

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -8,7 +8,31 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" :key="index">
+          <h1 class="title">{{ item.name }}</h1>
+          <ul>
+            <li v-for="(food,index) in item.foods" :key="index" class="content">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon"/>
+              </div>
+              <div class="food">
+                <h1 class="food-name">{{ food.name }}</h1>
+                <p class="food-des">{{ food.description }}</p>
+                <div class="food-sail">
+                  <span class="month-sail">月售{{ food.sellCount }}份</span><span>好评率{{ food.rating }}%</span>
+                </div>
+                <div>
+                  <span class="price">￥{{ food.price }}</span>
+                  <span class="old-price" v-show="food.oldPrice">￥{{ food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -44,10 +68,10 @@
     bottom: 46px
     width: 100%
     display: flex
+    overflow: hidden
     .menu-wrapper
       flex: 0 0 80px
       background-color: #f3f5f7
-      overflow: hidden
       .menu
         display: table
         height: 54px
@@ -64,5 +88,45 @@
             margin-right: 2px
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        font-size: 12px
+        border-left: 3px solid #d9dde1
+        color: rgb(147, 153, 159)
+        background-color: #f3f5f7
+      .content
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .food
+          flex: 1
+          .food-name
+            margin-bottom: 8px
+            padding-top: 2px
+            line-height: 14px;
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .food-des, .food-sail
+            margin-bottom: 7px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+            .month-sail
+              margin-right: 12px
+          .price
+            font-size: 14px
+            font-weight: 700
+            line-height: 24px
+            color: rgb(240, 20, 20)
+          .old-price
+            font-size: 10px
+            line-height: 24px
+            color: rgb(147, 153, 159)
 
 </style>
